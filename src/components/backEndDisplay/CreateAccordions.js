65ca5f0aeb828a73650373e6521f9d9fbf978dd0
Jack <jack@Jacks-MacBook-Air.local>
2019-03-08 17:48:29 -0500

    new commits

--- src/components/backEndDisplay/CreateAccordions.js
+++ src/components/backEndDisplay/CreateAccordions.js
@@ -4,8 +4,11 @@ import {TimelineMax, TweenLite, TweenMax} from "gsap/TweenMax";
 import {Elastic, Back, Power1, Power2, Linear, Bounce} from "gsap/all";
 
 const projects = [
-  {iden: 1, title: "PlinkoML with TensorFlow", description: "A JavaScript Machine Learning example that uses a K-Nearest Neighbors Algorithm (k-NN) to determine the result of the popular Family Feud game Plinko using TensorFlow and Nodejs.", pictures: ['javascript', 'nodejs'], show: "show", gitLink:"frontendSites/portfolio"},
-  {iden: 2, title: "Forecast App", description: "A complete NodeJS app that uses the DarkSky.net API to allow users to enter any location and instantly receive a forecast. ", pictures: ['javascript', 'nodejs', 'heroku'],link:"https://fast-anchorage-95119.herokuapp.com/"},
+  {iden: 1, title: "Task-App API", description: "A full REST API that allows users to create/read/update/delete an account with user profile images. Full authentication is provided. Logged in users can create/read/update/delete their very own tasks.", pictures: ['javascript', 'nodejs', 'mongodb', 'heroku'], gitLink:"https://github.com/fdo14/task-manager-api"},
+  {iden: 2, title: "PlinkoML with TensorFlow", description: "A JavaScript Machine Learning example that uses a K-Nearest Neighbors Algorithm (k-NN) to determine the result of the popular Family Feud game Plinko using TensorFlow and Nodejs.", pictures: ['javascript', 'nodejs'], show: "show", gitLink:"frontendSites/portfolio"},
+  {iden: 3, title: "Forecast App", description: "A complete NodeJS app that uses the DarkSky.net API to allow users to enter any location and instantly receive a forecast. ", pictures: ['javascript', 'nodejs', 'heroku'],link:"https://fast-anchorage-95119.herokuapp.com/"},
+  {iden: 4, title: "Bulls and Cows", description: "A C++ command line game. A fully functioning model of the famous Bull Cow Game - complete with ASCII graphics.", pictures: ['cplusplus'],gitLink:"https://github.com/fdo14?tab=repositories"},
+  {iden: 3, title: "Lissen", description: "An Android app designed to let you freely switch between your wifi and the network offered by TheHive.", pictures: ['java', 'android'],link:"https://github.com/fdo14/WifiChangerApp"},
 ]
 
 class CreateAccordions extends React.Component{
--- src/components/frontEndDisplay/CreateAccordions.js
+++ src/components/frontEndDisplay/CreateAccordions.js
@@ -4,15 +4,17 @@ import {TimelineMax, TweenLite, TweenMax} from "gsap/TweenMax";
 import {Elastic, Back, Power1, Power2, Linear, Bounce} from "gsap/all";
 
 const projects = [
-  {iden: 1, title: "Portfolio", description: "A cutomizable portfolio template that artists and creatives can use to showcase their work.", pictures: ['javascript', 'bootstrap', 'jquery', 'html5'], show: "show", link:"frontendSites/portfolio"},
-  {iden: 2, title: "TheHiveNet.com", description: "A website to showcase my start-up, TheHive. The website provides details on the company and highlights our win at the Social Venture Innovation Challenge.", pictures: ['javascript', 'bootstrap', 'html5'], link:"http://www.thehivenet.org"},
-  {iden: 3, title: "Streamy", description: "A React-Redux app that uses an RTMP media server to stream videos from your OBS account", pictures: ['react', ,'redux', 'javascript', 'html5', 'css3'], gitLink:"https://github.com/fdo14/twitch-clone"},
-  {iden: 4, title: "SigmaChiUMass.com", description: "A website I created as the first Web Master for the Sigma Chi chapter at UMass. The website has now moved on to a new Web Master.", pictures: ['javascript', 'bootstrap', 'jquery', 'html5'], link:"https://www.sigmachiumass.com"},
-  {iden: 5, title: "PlinkoML", description: "A JavaScript Machine Learning example that uses a K-Nearest Neighbors Algorithm (k-NN) to determine the result of the popular Family Feud game Plinko using lodash.", pictures: ['javascript', 'html5', 'css3'], link:"frontendSites/plinko"},
-  {iden: 6, title: "Greedy", description: "A JavaScript based dice game that allows two users to go head-to-head in a battle of wits.", pictures: ['javascript', 'html5', 'css3'], link:"frontendSites/greedy"},
-  {iden: 7, title: "Youtube Clone", description: "A React project using the Youtube API to create a video searching clone.", pictures: ['react', 'javascript', 'html5', 'css3'], reactLink:"/frontend/youtube"},
-  {iden: 8, title: "Landing Page", description: "The perfectly styled landing page for any start-up", pictures: ['bootstrap', 'javascript', 'html5', 'css3'], link:"frontendSites/landingPage"},
-  {iden: 9, title: "Season Display", description: "A React project using location tracking and React Hooks", pictures: ['react', 'javascript', 'html5', 'css3'], reactLink:"/frontend/seasons"}
+  {iden: 1, title: "Jackn.org", description: "My personal website that highlights my best work using animations and a React physics template.", pictures: ['javascript', 'bootstrap', 'jquery', 'html5', 'react'], show: "show", link:"https://www.jackn.org"},
+  {iden: 1, title: "Mean Gym", description: "An awesome landing page for any gym that wants a smoothly animated workout-scene as their home page. Using advanced SVG and CSS animations, this mean gym will taunt you into buying a membership.", pictures: ['javascript', 'bootstrap', 'jquery', 'html5', 'react'], show: "show", link:"https://nichols-mean-gym.herokuapp.com/"},
+  {iden: 2, title: "Portfolio", description: "A cutomizable portfolio template that artists and creatives can use to showcase their work.", pictures: ['javascript', 'bootstrap', 'jquery', 'html5'], show: "show", link:"frontendSites/portfolio"},
+  {iden: 3, title: "TheHiveNet.com", description: "A website to showcase my start-up, TheHive. The website provides details on the company and highlights our win at the Social Venture Innovation Challenge.", pictures: ['javascript', 'bootstrap', 'html5'], link:"http://www.thehivenet.org"},
+  {iden: 4, title: "Streamy", description: "A React-Redux app that uses an RTMP media server to stream videos from your OBS account", pictures: ['react', ,'redux', 'javascript', 'html5', 'css3'], gitLink:"https://github.com/fdo14/twitch-clone"},
+  {iden: 5, title: "SigmaChiUMass.com", description: "A website I created as the first Web Master for the Sigma Chi chapter at UMass. The website has now moved on to a new Web Master.", pictures: ['javascript', 'bootstrap', 'jquery', 'html5'], link:"https://www.sigmachiumass.com"},
+  {iden: 6, title: "PlinkoML", description: "A JavaScript Machine Learning example that uses a K-Nearest Neighbors Algorithm (k-NN) to determine the result of the popular Family Feud game Plinko using lodash.", pictures: ['javascript', 'html5', 'css3'], link:"frontendSites/plinko"},
+  {iden: 7, title: "Greedy", description: "A JavaScript based dice game that allows two users to go head-to-head in a battle of wits.", pictures: ['javascript', 'html5', 'css3'], link:"frontendSites/greedy"},
+  {iden: 8, title: "Youtube Clone", description: "A React project using the Youtube API to create a video searching clone.", pictures: ['react', 'javascript', 'html5', 'css3'], reactLink:"/frontend/youtube"},
+  {iden: 9, title: "Landing Page", description: "The perfectly styled landing page for any start-up", pictures: ['bootstrap', 'javascript', 'html5', 'css3'], link:"frontendSites/landingPage"},
+  {iden: 10, title: "Season Display", description: "A React project using location tracking and React Hooks", pictures: ['react', 'javascript', 'html5', 'css3'], reactLink:"/frontend/seasons"}
 
 ]
 
